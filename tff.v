559831bd2e905a05b2b9b69766f9f9fbaf6c0291
mads-88 <madisonhickey@u.boisestate.edu>
2026-03-31 17:10:30 -0600

    Update tff.v

--- tff.v
+++ tff.v
@@ -1,24 +1,24 @@
 module tff(
-    input T,
-    input clk,
-    output wire Q,
-    output wire Qnot
+input T,
+input clk,
+output wire Q,
+output wire Qnot
 );
 
-    wire D;
-    wire current_q;
-    wire current_qnot;
+wire D;
+wire current_q;
+wire current_qnot;
 
-    assign D = (T & current_qnot) | (~T & current_q);
+assign D = (T & current_qnot) | (~T & current_q);
 
-    dff dff1(
-        .D(D),
-        .clk(clk),
-        .Q(current_q),
-        .Qnot(current_qnot)
-    );
+dff dff1(
+.D(D),
+.clk(clk),
+.Q(current_q),
+.Qnot(current_qnot)
+);
 
-    assign Q = current_q;
-    assign Qnot = current_qnot;
+assign Q = current_q;
+assign Qnot = current_qnot;
 
 endmodule
