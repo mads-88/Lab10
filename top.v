78e8a2e78c6472418843e6c64d25980eb78f3a04
mads-88 <madisonhickey@u.boisestate.edu>
2026-03-31 17:11:02 -0600

    Update top.v

--- top.v
+++ top.v
@@ -1,29 +1,29 @@
 module top(
-    input [3:0] sw,
-    input btnC,
-    output [5:0] led
+input [3:0] sw,
+input btnC,
+output [5:0] led
 );
 
-    dff(
-        .D(sw[0]),
-        .clk(btnC),
-        .Q(led[0]),
-        .Qnot(led[1])
-    );
+dff dff_inst(
+.D(sw[0]),
+.clk(btnC),
+.Q(led[0]),
+.Qnot(led[1])
+);
 
-    jkff(
-        .J(sw[1]),
-        .K(sw[2]),
-        .clk(btnC),
-        .Q(led[2]),
-        .Qnot(led[3])
-    );
+jkff jkff_inst(
+.J(sw[1]),
+.K(sw[2]),
+.clk(btnC),
+.Q(led[2]),
+.Qnot(led[3])
+);
 
-    tff(
-        .T(sw[3]),
-        .clk(btnC),
-        .Q(led[4]),
-        .Qnot(led[5])
-    );
+tff tff_inst(
+.T(sw[3]),
+.clk(btnC),
+.Q(led[4]),
+.Qnot(led[5])
+);
 
 endmodule
