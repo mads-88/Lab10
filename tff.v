63b1ac350cd19d7c1cb880df5197e3e4a129699a
mads-88 <madisonhickey@u.boisestate.edu>
2026-03-31 16:50:37 -0600

    Create tff.v

---
+++ tff.v
@@ -0,0 +1,24 @@
+module tff(
+    input T,
+    input clk,
+    output wire Q,
+    output wire Qnot
+);
+
+    wire D;
+    wire current_q;
+    wire current_qnot;
+
+    assign D = (T & current_qnot) | (~T & current_q);
+
+    d_flip_flop dff_t(
+        .D(D),
+        .clk(clk),
+        .Q(current_q),
+        .Qnot(current_qnot)
+    );
+
+    assign Q = current_q;
+    assign Qnot = current_qnot;
+
+endmodule
